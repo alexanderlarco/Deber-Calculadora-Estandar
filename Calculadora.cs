c3a4c466bdb51a743c9a93fb83c57630c687b07c
alexander larco <52249910+alexanderlarco@users.noreply.github.com>
2020-12-10 21:49:49 -0500

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+
+namespace Revision
+
+{
+   
+     public class Calculadora
+   
+     {
+        
+//Metodo de la clase Calculadora
+
+       public string Restar(int numeroUno, string operacion, int numeroDos)
+       
+       {
+            
+          if (operacion == "-")
+
+          {
+                
+             return $"{numeroUno - numeroDos}";
+            
+          }
+            
+          return $"La operación ingresada: {operacion} no se puedo ejecutar";
+        
+       }
+   
+     }
+
+ }
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Linq;
+
+
+
+
+namespace Revision
+
+{
+    
+    class Program
+    
+    {
+        
+
+        static void Main(string[] args)
+        
+        {
+
+            
+
+             if (args.Any())
+            
+             {
+                
+                int numero1 = Convert.ToInt32(args[0]);
+                
+                string operacion = args[1];
+                
+                int numero2 = Convert.ToInt32(args[2]);
+
+
+                
+                Calculadora calculadora = new Calculadora();
+
+ 
+               
+                Console.WriteLine(calculadora.Operar(numero1, operacion, numero2));
+
+
+                
+                return;
+            
+             }
+
+   
+        
+             Console.WriteLine("Bienvenidos a la calculdora estandar del Yairac");
+            
+            
+             
+             Console.ReadKey();
+        
+       }
+    
+   }
+
+
+}
